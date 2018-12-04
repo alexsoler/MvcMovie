2bc8dfbcfe5d13cf432713d2d24d7d9e85b54fc1
alexsoler <40123834+alexsoler@users.noreply.github.com>
2018-12-04 17:08:31 -0600

    No debo volver a experimentar con git cuando no debo

--- Migrations/MvcMovieContextModelSnapshot.cs
+++ Migrations/MvcMovieContextModelSnapshot.cs
@@ -25,6 +25,8 @@ namespace MvcMovie.Migrations
 
                     b.Property<decimal>("Price");
 
+                    b.Property<string>("Rating");
+
                     b.Property<DateTime>("ReleaseDate");
 
                     b.Property<string>("Title");
